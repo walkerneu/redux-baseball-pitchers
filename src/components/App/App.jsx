db682e8054bb00fde395367a0e20a5d99fb8d34c
Walker Neudorff <walkerneudorff@gmail.com>
2023-11-28 21:42:23 -0600

    Added TotalCatchers and TotalPitchers components, moving things from app jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,7 @@
 import {useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import TotalPitchers from '../TotalPitchers/TotalPitchers';
+import TotalCatchers from '../TotalCatchers/TotalCatchers';
 
 function App() {
   const [currentPitcher, setCurrentPitcher] = useState('Maud Nelson');
@@ -46,8 +48,8 @@ function App() {
       <h1>Redux Baseball Pitchers</h1>
       <h2>On the Mound: {currentPitcher}</h2>
       <h2>Behind the Plate: {currentCatcher}</h2>
-      <div>Total Pitchers: {pitcherList.length}</div>
-      <div>Total Catchers: {catcherList.length}</div>
+      <TotalPitchers />
+      <TotalCatchers />
       <h3>All Pitchers</h3>
       <form onSubmit={handlePitcherSubmit}>
         <input
